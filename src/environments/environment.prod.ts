284a49787c4c4074953e758ac5c431512212eade
igor90ns <aleksic90@gmail.com>
2018-06-02 12:10:15 +0200

    basic navbar routing finished

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBaYAKJBMgb9YKB9SpaHzTlo-1Lj9daw5k",
+    authDomain: "webshop-d153d.firebaseapp.com",
+    databaseURL: "https://webshop-d153d.firebaseio.com",
+    projectId: "webshop-d153d",
+    storageBucket: "webshop-d153d.appspot.com",
+    messagingSenderId: "306608646030"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBaYAKJBMgb9YKB9SpaHzTlo-1Lj9daw5k",
+    authDomain: "webshop-d153d.firebaseapp.com",
+    databaseURL: "https://webshop-d153d.firebaseio.com",
+    projectId: "webshop-d153d",
+    storageBucket: "webshop-d153d.appspot.com",
+    messagingSenderId: "306608646030"
+  }
 };
