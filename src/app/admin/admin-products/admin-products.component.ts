7b5aecb8c18178d321725e9c04bbd79be7402724
igor90ns <aleksic90@gmail.com>
2018-06-18 20:13:37 +0200

    search box implemented

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,16 +1,34 @@
 import { ProductsService } from './../../products.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: {title: string}[];
+  filteredProducts: any[];
+  subscription:Subscription;
 
   constructor(private productsService:ProductsService) {
-    this.products$ = this.productsService.getAll();
+    this.subscription = this.productsService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
+   }
+
+   filter(query){
+    this.filteredProducts = (query) ?
+    this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+    this.products;
+
+   
+   }
+
+   
+
+   ngOnDestroy(){
+    this.subscription.unsubscribe();
    }
 
   ngOnInit() {
